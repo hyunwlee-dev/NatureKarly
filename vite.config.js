7ebc1ef9d4af1b821076f9aed45120e81202bcb9
hyunwlee-dev <hwlee3928@gmail.com>
2023-03-12 16:18:48 +0900

    :poop: [Bad code] storybook이 @: ./src 절대경로를 못읽습니다.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import reactPlugin from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import * as path from 'node:path';
+import { resolve } from 'node:path';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
@@ -11,8 +12,7 @@ export default defineConfig({
     cors: true,
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    // alias: {'@': path.resolve(__dirname, './src'),},
+    alias: [{ find: '@', replacement: resolve(__dirname, '.src')}]
   },
 });
